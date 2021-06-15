eb374e99d9ab93d46c4b9088e5cdd949c2cf536a
Denis Vasilenko <tuxtender@gmail.com>
2021-06-16 00:47:15 +0500

    Complete a numerical differentiation

--- assignments/assignment1/gradient_check.py
+++ assignments/assignment1/gradient_check.py
@@ -29,17 +29,24 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     # We will go through every dimension of x and compute numeric
     # derivative for it
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
-    while not it.finished:
-        ix = it.multi_index
-        analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = 0
 
-        # TODO compute value of numeric gradient of f to idx
-        if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
-            print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
-            return False
+    with it:
+      while not it.finished:
+          ix = it.multi_index
+          analytic_grad_at_ix = analytic_grad[ix]
 
-        it.iternext()
+          # Compute value of numeric gradient of f to idx
+          orig_x[ix] = x[ix] + delta
+          f0 =f(orig_x)[0]
+          orig_x[ix] = x[ix] - delta
+          f1 = f(orig_x)[0]
+          numeric_grad_at_ix = (f0 - f1)/(2*delta)
+
+          if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
+              print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
+              return False
+
+          it.iternext()
 
     print("Gradient check passed!")
     return True
